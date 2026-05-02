db32940b57a1c7bc0e763ea51a1923a261232cb2
Am01102007 <161891643+Am01102007@users.noreply.github.com>
2026-05-02 07:32:31 -0500

    Add mock EmailService to FlytrackApplicationTests

--- src/test/java/com/flytrack/FlytrackApplicationTests.java
+++ src/test/java/com/flytrack/FlytrackApplicationTests.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlytrackApplicationTests {
-
+	@MockBean
+    private EmailService emailService;
 	@Test
 	void contextLoads() {
 	}
